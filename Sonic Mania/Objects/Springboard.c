7b34c11d7f4025a3f1255804c42e4331ca4c0c6b
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-07-18 13:28:50 +1000

    spring into action dies from a heart attack

--- Sonic Mania/Objects/Springboard.c
+++ Sonic Mania/Objects/Springboard.c
@@ -4,243 +4,213 @@ ObjectSpringboard *Springboard;
 
 void Springboard_Update(void)
 {
-    /*EntitySpringboard *entity; // esi
-    char playerID;             // al
-    int v4;                    // eax
-    int v5;                    // ecx
-    int v6;                    // edi
-    unsigned int v7;           // eax
-    int v8;                    // ecx
-    int v9;                    // edi
-    int v11;                   // ebx
-    int v12;                   // eax
-    int v13;                   // eax
-    int v14;                   // eax
-    int v15;                   // eax MAPDST
-    bool32 v16;                  // zf
-    signed int v17;            // eax
-    unsigned __int8 v18;       // al
-    Hitbox *v19;               // eax
-    Hitbox *v20;               // ecx
-    int v21;                   // eax
-    int v22;                   // eax
-    signed int v23;            // eax
-    Hitbox *v24;               // eax
-    Hitbox *v25;               // ecx
-    Hitbox Hitbox;             // [esp+Ch] [ebp-20h] BYREF
-    int v27;                   // [esp+14h] [ebp-18h]
-    int v29;                   // [esp+1Ch] [ebp-10h]
-    int v30;                   // [esp+20h] [ebp-Ch]
-    EntityPlayer *player;      // [esp+28h] [ebp-4h] MAPDST BYREF
-
     RSDK_THIS(Springboard);
     RSDK.ProcessAnimation(&entity->animator);
-    player = 0;
-    while (RSDK.GetActiveEntities(Player->objectID, &player) == true) {
-        playerID = RSDK.GetEntityID(player);
-        if (player->velocity.y >= 0 && ((1 << playerID) & entity->playerBits) != 0) {
-            v4 = (player->position.x - entity->position.x + 0x1C0000) >> 17;
-            if (v4 > 28)
-                v4 = 28;
-            v5 = 0;
-            if (v4 >= 0)
-                v5 = v4;
-            v6 = 28 - v5;
-            if ((entity->direction & 1) == 0)
-                v6 = v5;
-            if (v6 >= 8) {
+
+    foreach_active(Player, playerPtr)
+    {
+        if (playerPtr->velocity.y >= 0 && ((1 << RSDK.GetEntityID(playerPtr)) & entity->playerBits)) {
+            int val  = min((playerPtr->position.x - entity->position.x + 0x1C0000) >> 17, 28);
+            int val2 = max(val, 0);
+
+            int val3 = val2;
+            if ((entity->direction & 1))
+                val3 = 28 - val2;
+            if (val3 >= 8) {
                 if (entity->animator.frameID == 3)
-                    RSDK.SetSpriteAnimation(Springboard->animID, 0, &entity->animator, true, 0);
+                    RSDK.SetSpriteAnimation(Springboard->aniFrames, 0, &entity->animator, true, 0);
                 if (entity->animator.frameID == 2) {
-                    v7 = player->playerAnimator.animationID;
-                    if (v7 == 5 || v7 > 6 && v7 <= 9)
-                        player->storedAnim = player->playerAnimator.animationID;
+                    int anim = playerPtr->playerAnimator.animationID;
+                    if (anim == ANI_WALK || anim > ANI_AIRWALK && anim <= ANI_DASH)
+                        playerPtr->storedAnim = playerPtr->playerAnimator.animationID;
                     else
-                        player->storedAnim = ANI_WALK;
-                    player->state               = Player_State_Air;
-                    player->onGround       = false;
-                    player->tileCollisions = true;
-                    RSDK.SetSpriteAnimation(player->spriteIndex, 23, &player->playerAnimator, true, 1);
-                    player->groundVel = player->velocity.x;
-                    v8                     = 2 * v6 - 16;
-                    if (v8 > 39)
-                        v8 = 39;
-                    player->velocity.y = Springboard->array2[v8] - player->gravityStrength - entity->force;
-                    player->jumpAbility     = 0;
-                    RSDK.PlaySFX(Springboard->sfxID, 0, 255);
+                        playerPtr->storedAnim = ANI_WALK;
+                    playerPtr->state          = Player_State_Air;
+                    playerPtr->onGround       = false;
+                    playerPtr->tileCollisions = true;
+                    RSDK.SetSpriteAnimation(playerPtr->spriteIndex, ANI_SPRINGCS, &playerPtr->playerAnimator, true, 1);
+                    playerPtr->groundVel   = playerPtr->velocity.x;
+                    int pos                = min(2 * val3 - 16, 39);
+                    playerPtr->velocity.y  = Springboard->array2[pos] - playerPtr->gravityStrength - entity->force;
+                    playerPtr->jumpAbility = 0;
+                    RSDK.PlaySFX(Springboard->sfxSpring, 0, 255);
                 }
             }
         }
     }
-    player = 0;
-    if (RSDK.GetActiveEntities(Player->objectID, &player) == true) {
-        v9 = v27;
-        while (1) {
-            playerID = RSDK.GetEntityID(player);
-            v11 = 0;
-            v30 = player->groundVel;
-            v12 = player->position.x - entity->position.x + 1835008;
-            v29 = player->velocity.x;
-            v13 = v12 >> 17;
-            if (v13 >= 0)
-                v11 = v13;
-            if (v11 > 27)
-                v11 = 27;
-            if (!entity->direction)
-                break;
-            if (entity->direction == 1) {
-                v14 = entity->animator.frameID;
-                v11 = abs32(v11 - 27);
-                if (v14 >= 0) {
-                    if (v14 <= 2) {
-                        v15 = Springboard->array4[v11];
-                    }
-                    else if (v14 == 3) {
-                        v15 = Springboard->array3[v11];
-                    }
-                }
-                *&Hitbox.right = -524260;
-                Hitbox.top     = -v15;
-                Hitbox.left    = -28;
-                v16            = ((1 << playerID) & entity->playerBits) == 0;
-                v27            = 1 << playerID;
-                if (v16)
-                    v17 = Player_CheckCollisionBox(player, entity, &Hitbox);
-                else
-                    v17 = Player_CheckCollisionPlatform(player, entity, &Hitbox);
-                v9 = v17;
-                switch (v17) {
-                    case 0:
-                    case 2:
-                    case 4: break;
-                    case 1: goto LABEL_40;
-                    case 3:
-                        player->groundVel  = v30;
-                        player->velocity.x = v29;
-                        break;
-                    default: goto LABEL_42;
-                }
-                if (player->velocity.y >= 0 && (v27 & entity->playerBits) != 0) {
-                    v19 = RSDK.GetHitbox(&player->playerAnimator, 0);
-                    v20 = &defaultHitbox;
-                    if (v19)
-                        v20 = v19;
-                    player->position.y = entity->position.y - (v20->bottom << 16) - (v15 << 16);
-                    if (v17)
-                    LABEL_72:
-                        v17 = true;
-                    else
-                        v17 = player->position.x < entity->position.x;
-                }
-            LABEL_43:
-                player->flailing = 0;
+
+    foreach_active(Player, player)
+    {
+        int playerID   = RSDK.GetEntityID(player);
+        int playerVel  = player->groundVel;
+        int playerVelX = player->velocity.x;
+        bool32 flag    = false;
+        int val        = clampVal((player->position.x - entity->position.x + 0x1C0000) >> 17, 0, 27);
+
+        if (!entity->direction) {
+            int hitboxTop = 0;
+
+            if (entity->animator.frameID < 0) {
+                hitboxTop = hitboxTop;
             }
-            v18 = entity->playerBits;
-            if (v9 == 1) {
-                entity->playerBits = v18 | (1 << playerID);
-                if (v11 >= 8 && !entity->field_5C)
-                    entity->field_5C = 6;
+            else if (entity->animator.frameID <= 2) {
+                hitboxTop = Springboard->array4[val];
+            }
+            else if (entity->animator.frameID == 3) {
+                hitboxTop = Springboard->array3[val];
             }
             else {
-                entity->playerBits = v18 & ~(1 << playerID);
+                hitboxTop = hitboxTop;
             }
-            if (RSDK.GetActiveEntities(Player->objectID, &player) != true)
-                return;
-        }
-        v21 = entity->animator.frameID;
-        if (v21 >= 0) {
-            if (v21 <= 2) {
-                v22 = Springboard->array4[v11];
-                v15 = v22;
-                goto LABEL_61;
-            }
-            if (v21 == 3) {
-                v22 = Springboard->array3[v11];
-                v15 = v22;
-            LABEL_61:
-                *&Hitbox.right = -524260;
-                Hitbox.top     = -v22;
-                Hitbox.left    = -28;
-                v16            = ((1 << playerID) & entity->playerBits) == 0;
-                v27            = 1 << playerID;
-                if (v16)
-                    v23 = Player_CheckCollisionBox(player, entity, &Hitbox);
-                else
-                    v23 = Player_CheckCollisionPlatform(player, entity, &Hitbox);
-                v9 = v23;
-                switch (v23) {
-                    case 0:
-                    case 3:
-                    case 4: goto LABEL_66;
-                    case 1:
-                    LABEL_40:
-                        if (player->state != Player_State_MightyHammerDrop)
-                            goto LABEL_43;
+
+            Hitbox hitbox;
+            hitbox.left   = -28;
+            hitbox.top    = -hitboxTop;
+            hitbox.right  = 28;
+            hitbox.bottom = 8;
+
+            byte collision = 0;
+            if (!((1 << playerID) & entity->playerBits))
+                collision = Player_CheckCollisionBox(player, entity, &hitbox);
+            else
+                collision = Player_CheckCollisionPlatform(player, entity, &hitbox);
+            flag = collision;
+            switch (collision) {
+                case 2: player->groundVel = playerVel; player->velocity.x = playerVelX;
+                case 0:
+                case 3:
+                case 4:
+                    if (player->velocity.y >= 0 && ((1 << playerID) & entity->playerBits)) {
+                        Hitbox *playerHitbox = Player_GetHitbox(player);
+                        player->position.y   = entity->position.y - (playerHitbox->bottom << 16) - (hitboxTop << 16);
+                        if (!flag)
+                            flag = player->position.x > entity->position.x;
+                    }
+                    break;
+                case 1:
+                    if (player->state == Player_State_MightyHammerDrop) {
                         player->state = Player_State_Air;
-                    LABEL_42:
-                        break;
-                    case 2:
-                        player->groundVel  = v30;
-                        player->velocity.x = v29;
-                    LABEL_66:
-                        if (player->velocity.y >= 0 && (v27 & entity->playerBits) != 0) {
-                            v24 = RSDK.GetHitbox(&player->playerAnimator, 0);
-                            v25 = &defaultHitbox;
-                            if (v24)
-                                v25 = v24;
-                            player->position.y = entity->position.y - (v25->bottom << 16) - (v15 << 16);
-                            if (v9 || player->position.x > entity->position.x)
-                                goto LABEL_72;
-                        }
-                        goto LABEL_43;
-                    default: goto LABEL_42;
-                }
-                goto LABEL_43;
+                    }
+                    break;
+                default: break;
             }
+            player->flailing = false;
         }
-        LOWORD(v22) = v15;
-        goto LABEL_61;
-    }//*/
-    
+        else if (entity->direction == FLIP_X) {
+            int pos       = abs(val - 27);
+            int hitboxTop = 0;
+
+            if (entity->animator.frameID < 0) {
+                hitboxTop = hitboxTop;
+            }
+            else if (entity->animator.frameID <= 2) {
+                hitboxTop = Springboard->array4[pos];
+            }
+            else if (entity->animator.frameID != 3) {
+                hitboxTop = Springboard->array3[pos];
+            }
+            else {
+                hitboxTop = hitboxTop;
+            }
+
+            Hitbox hitbox;
+            hitbox.left   = -28;
+            hitbox.top    = -hitboxTop;
+            hitbox.right  = 28;
+            hitbox.bottom = 8;
+
+            byte collision = 0;
+            if (!((1 << playerID) & entity->playerBits))
+                collision = Player_CheckCollisionBox(player, entity, &hitbox);
+            else
+                collision = Player_CheckCollisionPlatform(player, entity, &hitbox);
+            flag = collision;
+            switch (collision) {
+                case 0:
+                case 2:
+                case 4: break;
+                case 1:
+                    if (player->state == Player_State_MightyHammerDrop) {
+                        player->state = Player_State_Air;
+                    }
+                    break;
+                case 3:
+                    player->groundVel  = playerVel;
+                    player->velocity.x = playerVelX;
+                    break;
+                default: break;
+            }
+
+            if (player->velocity.y >= 0 && ((1 << playerID) & entity->playerBits)) {
+                Hitbox *playerHitbox = Player_GetHitbox(player);
+                player->position.y   = entity->position.y - (playerHitbox->bottom << 16) - (hitboxTop << 16);
+                if (!flag)
+                    flag = player->position.x < entity->position.x;
+            }
+            player->flailing = false;
+        }
+
+        if (flag) {
+            entity->playerBits |= (1 << playerID);
+            if (val >= 8 && !entity->field_5C)
+                entity->field_5C = 6;
+        }
+        else {
+            entity->playerBits &= ~(1 << playerID);
+        }
+    }
 }
 
-void Springboard_LateUpdate(void)
-{
+void Springboard_LateUpdate(void) {}
 
-}
-
-void Springboard_StaticUpdate(void)
-{
-
-}
+void Springboard_StaticUpdate(void) {}
 
 void Springboard_Draw(void)
 {
-
+    RSDK_THIS(Springboard);
+    RSDK.DrawSprite(&entity->animator, NULL, false);
 }
 
-void Springboard_Create(void* data)
+void Springboard_Create(void *data)
 {
-
+    RSDK_THIS(Springboard);
+    entity->visible   = true;
+    entity->drawOrder = Zone->drawOrderLow;
+    entity->drawFX |= FX_FLIP;
+    entity->active        = ACTIVE_BOUNDS;
+    entity->updateRange.x = 0x400000;
+    entity->updateRange.y = 0x400000;
+    if (!RSDK_sceneInfo->inEditor) {
+        entity->force = (entity->force + 8) << 15;
+        RSDK.SetSpriteAnimation(Springboard->aniFrames, 0, &entity->animator, true, 3);
+    }
 }
 
 void Springboard_StageLoad(void)
 {
-
+    Springboard->aniFrames = RSDK.LoadSpriteAnimation("CPZ/Springboard.bin", SCOPE_STAGE);
+    Springboard->sfxSpring = RSDK.GetSFX("Global/Spring.wav");
+    DEBUGMODE_ADD_OBJ(Springboard);
 }
 
-void Springboard_EditorDraw(void)
+void Springboard_DebugSpawn(void)
 {
-
+    RSDK_THIS(Springboard);
+    CREATE_ENTITY(Springboard, NULL, entity->position.x, entity->position.y);
 }
-
-void Springboard_EditorLoad(void)
+void Springboard_DebugDraw(void)
 {
-
+    RSDK_THIS(Springboard);
+    RSDK.SetSpriteAnimation(Springboard->aniFrames, 1, &DebugMode->debugData, true, 0);
+    RSDK.DrawSprite(&DebugMode->debugData, 0, false);
 }
+
+void Springboard_EditorDraw(void) {}
+
+void Springboard_EditorLoad(void) {}
 
 void Springboard_Serialize(void)
 {
-
+    RSDK_EDITABLE_VAR(Springboard, VAR_UINT8, direction);
+    RSDK_EDITABLE_VAR(Springboard, VAR_ENUM, force);
 }
-
--- Sonic Mania/Objects/Springboard.h
+++ Sonic Mania/Objects/Springboard.h
@@ -10,8 +10,8 @@ typedef struct {
     int array2[40]; //= { 0, 0, 0, 0, 0, 0, 0, 0, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -65536, -131072, -131072, -131072, -131072, -131072, -131072, -131072, -131072, -196608, -196608, -196608, -196608, -196608, -196608, -262144, -262144 };
     int array3[28]; //= { 8, 9, 10, 11, 12, 13, 14, 15, 16, 16, 17, 18, 19, 20, 20, 21, 21, 22, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24 };
     int array4[28]; //= { 8, 9, 10, 11, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 14, 14, 15, 15, 16, 16, 16, 16, 15, 15, 14, 14, 13, 13 };
-    ushort animID;
-    ushort sfxID;
+    ushort aniFrames;
+    ushort sfxSpring;
 } ObjectSpringboard;
 
 // Entity Class
@@ -38,6 +38,7 @@ void Springboard_EditorLoad(void);
 void Springboard_Serialize(void);
 
 // Extra Entity Functions
-
+void Springboard_DebugSpawn(void);
+void Springboard_DebugDraw(void);
 
 #endif //!OBJ_SPRINGBOARD_H
