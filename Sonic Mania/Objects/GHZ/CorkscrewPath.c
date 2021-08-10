4061057acd905441d786633b79284cd2ab9c7b2d
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-08-10 13:54:51 +1000

    fixed this in this house we stan clean code

--- Sonic Mania/Objects/GHZ/CorkscrewPath.c
+++ Sonic Mania/Objects/GHZ/CorkscrewPath.c
@@ -7,8 +7,9 @@ void CorkscrewPath_Update(void)
     RSDK_THIS(CorkscrewPath);
     foreach_active(Player, player)
     {
+        int playerID = RSDK.GetEntityID(player);
         if (abs(entity->position.x - player->position.x) >> 16 > entity->periodShifted) {
-            entity->activePlayers &= ~RSDK.GetEntityID(player);
+            entity->activePlayers &= ~playerID;
             if (player->playerAnimator.animationID != ANI_SPRINGCS)
                 player->direction &= ~FLIP_Y;
         }
@@ -16,23 +17,19 @@ void CorkscrewPath_Update(void)
             int x     = entity->periodShifted + ((player->position.x - entity->position.x) >> 16);
             int frame = 24 * x / entity->period;
             int arc   = entity->amplitude * RSDK.Cos1024((x << 10) / entity->period);
-            if (!(RSDK.GetEntityID(player) & entity->activePlayers)) {
+            if (!(playerID & entity->activePlayers)) {
                 if (abs(arc + entity->position.y - player->position.y) >= 0x100000) {
-                    entity->activePlayers &= ~RSDK.GetEntityID(player);
+                    entity->activePlayers &= ~playerID;
                 }
                 else {
-                    if (abs(player->groundVel) <= 0x40000 || player->velocity.y <= -0x40000 || !player->groundedStore) {
-                        entity->activePlayers &= ~RSDK.GetEntityID(player);
-                    }
-                    else {
+                    if (abs(player->groundVel) > 0x40000 && player->velocity.y > -0x40000 && player->groundedStore) {
                         player->position.y = arc + entity->position.y;
                         player->velocity.y = 0;
                         player->onGround   = true;
                         if (player->playerAnimator.animationID != ANI_JUMP) {
                             if (player->groundVel < 0) {
                                 player->direction |= FLIP_Y;
-                                RSDK.SetSpriteAnimation(player->spriteIndex, ANI_SPRINGCS, &player->playerAnimator, true,
-                                                        CorkscrewPath->frameTable[frame]);
+                                RSDK.SetSpriteAnimation(player->spriteIndex, ANI_SPRINGCS, &player->playerAnimator, true, CorkscrewPath->frameTable[frame]);
                             }
                             else {
                                 player->direction &= ~FLIP_Y;
@@ -40,18 +37,20 @@ void CorkscrewPath_Update(void)
                             }
                         }
                     }
+                    else {
+                        entity->activePlayers &= ~playerID;
+                    }
                 }
             }
             else if (abs(player->groundVel) > 0x40000 && player->groundedStore && abs(arc + player->position.y - player->position.y) < 0x100000) {
-                entity->activePlayers |= RSDK.GetEntityID(player);
+                entity->activePlayers |= playerID;
                 player->position.y = arc + entity->position.y;
                 player->velocity.y = 0;
                 player->onGround   = true;
                 if (player->playerAnimator.animationID != ANI_JUMP) {
                     if (player->groundVel < 0) {
                         player->direction |= FLIP_Y;
-                        RSDK.SetSpriteAnimation(player->spriteIndex, ANI_SPRINGCS, &player->playerAnimator, true,
-                                                CorkscrewPath->frameTable[frame]);
+                        RSDK.SetSpriteAnimation(player->spriteIndex, ANI_SPRINGCS, &player->playerAnimator, true, CorkscrewPath->frameTable[frame]);
                     }
                     else {
                         player->direction &= ~FLIP_Y;
