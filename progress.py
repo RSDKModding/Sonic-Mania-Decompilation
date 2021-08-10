267a3558048cc45c277acec290a0cf49d28112dc
RMGRich <rmgrichdeveloper@gmail.com>
2021-08-10 16:12:01 -0500

    PROGRESS.PY!!! !LIkeee uhhhhhhhhhhhhh yeahhhh

---
+++ progress.py
@@ -0,0 +1,26 @@
+import os, re
+d = {}
+completed = 0
+count = 0
+p = "Sonic Mania/Objects/"
+for x in os.listdir(p):
+    if os.path.isdir(p + x):
+        d.update({x: []})
+        for y in os.listdir(p + x):
+            if y.endswith(".h"): continue
+            c = not bool(re.search(
+                r"void .*_Create\(void\* data\).*(\n|\s)?\{\n?\n?\}", open(p + x + "/" + y).read()))
+            d[x].append((y[:-2], c))
+            if c:
+                completed += 1
+            count += 1
+
+for x in d:
+    print(x)
+    for n, b in d[x]:
+        print(f"- [{'x' if b else ' '}] `{n}`")
+    
+print(completed / count * 100)
+
+
+
