aad8dcf9d577789202ca3dda602416c7c26b2a45
Rubberduckycooly <rubberduckycooly@outlook.com>
2021-10-10 10:55:53 +1100

    sawybuzz

--- Sonic Mania/Objects/MMZ/BuzzSaw.h
+++ Sonic Mania/Objects/MMZ/BuzzSaw.h
@@ -5,12 +5,24 @@
 
 // Object Class
 typedef struct {
-	RSDK_OBJECT
+    RSDK_OBJECT
+    ushort aniFrames;
+    Hitbox hitbox;
+    int field_10;
+    int field_14;
+    int field_18;
 } ObjectBuzzSaw;
 
 // Entity Class
 typedef struct {
-	RSDK_ENTITY
+    RSDK_ENTITY
+    StateMachine(state);
+    int type;
+    Vector2 amplitude;
+    int speed;
+    Vector2 startPos;
+    Vector2 drawPos;
+    Animator animator;
 } EntityBuzzSaw;
 
 // Object Struct
@@ -28,6 +40,15 @@ void BuzzSaw_EditorLoad(void);
 void BuzzSaw_Serialize(void);
 
 // Extra Entity Functions
+bool32 BuzzSaw_CheckCB(void);
+void BuzzSaw_UpdateCB(int sfx);
 
+void BuzzSaw_CheckPlayerCollisions(void);
+
+void BuzzSaw_State_OnArm(void);
+
+void BuzzSaw_State_FreeMove_Waiting(void);
+void BuzzSaw_State_FreeMove_Released(void);
+void BuzzSaw_State_FreeMove_Reset(void);
 
 #endif //!OBJ_BUZZSAW_H
