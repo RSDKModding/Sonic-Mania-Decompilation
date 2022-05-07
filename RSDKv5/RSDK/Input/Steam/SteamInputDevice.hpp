fc75b507555ab6940641aef960512f48ec97abd7
Rubberduckycooly <rubberduckycooly@outlook.com>
2022-05-07 14:56:32 +1000

    RSDK-Specific SteamInputDevice stuff

---
+++ RSDKv5/RSDK/Input/Steam/SteamInputDevice.hpp
@@ -0,0 +1,27 @@
+
+
+struct InputDeviceSteam : InputDevice {
+    void UpdateInput(void);
+    void ProcessInput(int32 controllerID);
+
+    int32 keyMasks[9];
+
+    uint16 inputFlags;
+    uint16 prevInputFlags;
+    uint8 controllerID;
+    uint8 stateUp;
+    uint8 stateDown;
+    uint8 stateLeft;
+    uint8 stateRight;
+    uint8 stateA;
+    uint8 stateB;
+    uint8 stateC;
+    uint8 stateX;
+    uint8 stateY;
+    uint8 stateZ;
+    uint8 stateStart;
+    uint8 stateSelect;
+};
+
+void InitSteamInputAPI();
+InputDeviceSteam *InitSteamInputDevice(uint32 id);
