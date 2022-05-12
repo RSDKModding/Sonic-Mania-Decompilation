493416a469a996c7f6150774bca56aadf7cf06a8
Rubberduckycooly <rubberduckycooly@outlook.com>
2022-05-12 18:15:15 +1000

    perfect

--- RSDKv5/Shaders/DX9/YUV-422.hlsl
+++ RSDKv5/Shaders/DX9/YUV-422.hlsl
@@ -2,13 +2,15 @@
 // VARIABLES
 // =======================
 
+SamplerState texDiffuse: register(s0);  // screen display texture
+
 float2 pixelSize: register(c0);         // internal game resolution (usually 424x240 or smth)
+float2 textureSize: register(c1);       // size of the internal framebuffer texture
+float2 viewSize: register(c2);          // window viewport size
+
 #if defined(RETRO_REV02)                // if RETRO_REV02 is defined it assumes the engine is plus/rev02 RSDKv5, else it assumes pre-plus/Rev01 RSDKv5
 float2 screenDim: register(c3);         // screen dimming percent
 #endif
-SamplerState texDiffuse: register(s0);  // screen display texture
-float2 textureSize: register(c1);       // size of the internal framebuffer texture
-float2 viewSize: register(c2);          // window viewport size
 
 
 // =======================
