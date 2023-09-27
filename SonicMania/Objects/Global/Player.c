9cddea8e4fb557c99fa6a445846a98b0584c24ec
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2023-09-28 00:03:26 +0200

    Player: Fix Knuckles not dropping from retracting spikes

--- SonicMania/Objects/Global/Player.c
+++ SonicMania/Objects/Global/Player.c
@@ -5135,22 +5135,7 @@ void Player_State_KnuxWallClimb(void)
             if (RSDK.ObjectTileCollision(self, self->collisionLayers, CMODE_ROOF, self->collisionPlane, roofX, roofY, true))
                 self->velocity.y = 0;
 
-            if (collidedHigh && collidedLow) {
-                if (!self->velocity.y)
-                    RSDK.SetSpriteAnimation(self->aniFrames, ANI_CLIMB_IDLE, &self->animator, false, 0);
-                else if (self->velocity.y > 0)
-                    RSDK.SetSpriteAnimation(self->aniFrames, ANI_CLIMB_DOWN, &self->animator, false, 0);
-                else if (self->velocity.y < 0)
-                    RSDK.SetSpriteAnimation(self->aniFrames, ANI_CLIMB_UP, &self->animator, false, 0);
-
-                self->velocity.y = 0;
-            }
-            else if (collidedHigh) {
-                RSDK.SetSpriteAnimation(self->aniFrames, ANI_GLIDE_DROP, &self->animator, false, 2);
-                self->velocity.y = 0;
-                self->state      = Player_State_KnuxGlideDrop;
-            }
-            else if (collidedLow) {
+            if (!collidedHigh) {
                 self->position.y &= 0xFFF00000;
                 if (self->isChibi)
                     self->position.y -= 0x10000;
@@ -5163,6 +5148,21 @@ void Player_State_KnuxWallClimb(void)
                 self->timer          = 1;
                 self->tileCollisions = TILECOLLISION_NONE;
                 self->velocity.y     = 0;
+            }
+            else if (!collidedLow) {
+                RSDK.SetSpriteAnimation(self->aniFrames, ANI_GLIDE_DROP, &self->animator, false, 2);
+                self->velocity.y = 0;
+                self->state      = Player_State_KnuxGlideDrop;
+            }
+            else {
+                if (!self->velocity.y)
+                    RSDK.SetSpriteAnimation(self->aniFrames, ANI_CLIMB_IDLE, &self->animator, false, 0);
+                else if (self->velocity.y > 0)
+                    RSDK.SetSpriteAnimation(self->aniFrames, ANI_CLIMB_DOWN, &self->animator, false, 0);
+                else if (self->velocity.y < 0)
+                    RSDK.SetSpriteAnimation(self->aniFrames, ANI_CLIMB_UP, &self->animator, false, 0);
+
+                self->velocity.y = 0;
             }
         }
     }
