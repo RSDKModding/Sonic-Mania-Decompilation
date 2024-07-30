6e9550502a19e25497c9e8b4604bb0b0e2fd0bf3
MegAmi <angrybirdconnor@gmail.com>
2024-07-30 15:18:38 -0500

    Fix several issues in funcheader.py
    - Changed arguments error to specify that a path to the Objects folder is required
    - Fixed some functions (namely `DebugDraw` functions) being mistaken as events
    - Fixed pre-processors being written before object headers
    - Fixed a non-preprocessor function being written before an `#endif` line
    - Fixed functions not being added if they are separated into multiple lines or have a comment at the end

--- SonicMania/funcheader.py
+++ SonicMania/funcheader.py
@@ -4,7 +4,7 @@ from typing import IO, BinaryIO, Dict, Iterable, Iterator, List, Tuple
 from pathlib import Path
 
 if len(sys.argv) < 2:
-    print(f"Not enough arguments supplied... Exiting...")
+    print(f"No Objects path specified... Exiting...")
     exit(1)
 
 events = ("Update", "LateUpdate", "StaticUpdate", "Draw", "Create", "StageLoad", "EditorLoad", "EditorDraw", "Serialize")
@@ -16,20 +16,27 @@ for path in Path(sys.argv[1]).rglob("*.h"):
     hasPrepos = False
     f = open(path, "r")
     while (line := f.readline()) != "":
-        if (line != line.lstrip()): 
+        if (line != line.lstrip() or line.startswith("//")): 
             continue
         line = line.rstrip()
-        if (match := re.fullmatch(r"([a-zA-Z0-9]* ?\**) *([^(]*)\((.*)\);", line)) != None:
-            ret, name, args = match.groups()
-            if name.endswith(events):
+        if (match := re.fullmatch(r"([a-zA-Z0-9]* ?\**) *([^(]*)\((.*)(,|\);)(\s*\/\/.*)?", line)) != None:
+            ret, name, args, end, comment = match.groups()
+            if name[len(path.stem) + 1:] in events:
                 continue
+            if (not done): 
+                print(f"    // {path.parent.name}/{path.stem}")
+                done = True
+                if (prepros == "#endif"):
+                    prepros = ""
+                    hasPrepos = False
             if prepros and (prepros != "#endif"):
                 print(prepros)
                 prepros = ""
                 hasPrepos = True
-            if (not done): 
-                print(f"    // {path.parent.name}/{path.stem}")
-                done = True
+            if (prepros == "#endif" and hasPrepos == True):
+                print(prepros)
+                prepros = ""
+                hasPrepos = False
             print(f"    ADD_PUBLIC_FUNC({name});")
             if (prepros == "#endif"):
                 print(prepros)
