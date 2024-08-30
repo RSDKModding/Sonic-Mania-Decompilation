eca21d359cc1d5d4705d084041a835ca9f157ca3
Mefiresu <15063879+Mefiresu@users.noreply.github.com>
2024-08-30 21:50:55 +0200

    Add missing TYPE_DEVOUTPUT default object

--- SonicMania/GameLink.h
+++ SonicMania/GameLink.h
@@ -649,7 +649,12 @@ typedef enum { FX_NONE = 0, FX_FLIP = 1, FX_ROTATE = 2, FX_SCALE = 4 } DrawFX;
 
 typedef enum { FLIP_NONE, FLIP_X, FLIP_Y, FLIP_XY } FlipFlags;
 
-typedef enum { TYPE_BLANK } DefaultObjTypes;
+typedef enum {
+    TYPE_BLANK,
+#if RETRO_REV02
+    TYPE_DEVOUTPUT,
+#endif
+} DefaultObjTypes;
 
 typedef enum {
     INPUT_UNASSIGNED = -2,
